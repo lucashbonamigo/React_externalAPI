bcb333a0b202b60f931b18f02eb5c35490fda60e
Lucas Oliveira <lucashbo321@gmail.com>
2026-01-25 16:37:01 -0300

    adição do botão limpar pesquisa e correção em navegadores diferentes

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import logo from "../../../public/logo.jpg";
 import { useDispatch } from 'react-redux';
 import { setMoviesList } from '../../redux/actions';
-import useAxios from "../../hooks/useAxios"; 
+import useAxios from "../../hooks/useAxios";
 
 export const NavBar = () => {
     const [find, setFind] = useState("");
@@ -23,22 +23,27 @@ export const NavBar = () => {
 
     return (
         <nav className="flex w-full h-17 bg-black p-3">
-            <div className="flex max-w-300 mx-auto">
+            <div className="flex place-content-between w-300 max-w-300 mx-auto">
                 <div>
                     <img className="h-full" src={logo} alt="logo" />
                 </div>
 
                 <div className="flex place-content-between border-white">
-                    <input 
-                        type="text" 
-                        onChange={(e) => setFind(e.target.value)} 
-                        className="rounded-sm bg-white mr-1 pl-1" 
-                    />
-                    <input 
-                        type="button" 
-                        onClick={() => searchMovie()} 
-                        value={loading ? "Carregando..." : "Pesquisar"} 
-                        className="bg-yellow-500 hover:bg-yellow-700 p-2 rounded-sm cursor-pointer" 
+                    <div className="input-container">
+                        <input
+                            type="text"
+                            onChange={(e) => setFind(e.target.value)}
+                            value={find}
+                            className="rounded-sm bg-white mr-1 pl-1 h-full"
+                            id="campoTexto"
+                        />
+                        {find ? <span id="limpar" onClick={() => setFind('')}>&times;</span> : null}
+                    </div>
+                    <input
+                        type="button"
+                        onClick={() => searchMovie()}
+                        value={loading ? "Carregando..." : "Pesquisar"}
+                        className="bg-yellow-500 hover:bg-yellow-700 p-2 rounded-sm cursor-pointer"
                     />
                 </div>
             </div>
