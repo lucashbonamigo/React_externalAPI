6a7166aab899ba1b8a450fa6c7589301f7a94d3f
Lucas Oliveira <lucashbo321@gmail.com>
2026-01-25 16:26:44 -0300

    deploy

--- src/components/routes/AppRoutes.tsx
+++ src/components/routes/AppRoutes.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import App from "../../App";
 import Movie from "../Movie";
 
 
 export const AppRoutes = () => {
 
-    return(
-        <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<App />} />
-            <Route path='/movie/:id' element={<Movie />} />
-        </Routes>
-    </BrowserRouter>
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path='/' element={<App />} />
+                <Route path='/movie/:id' element={<Movie />} />
+            </Routes>
+        </HashRouter>
     );
 
 };
